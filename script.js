a3f5fb5ee6cb828c08b4310a693060d2d825c384
SwollenClackers <torrentbigswollennuts@gmail.com>
2024-12-12 22:35:16 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// You can customize the message here
+const message = "Isaac Reynolds is a fake aryan! He has dyed blonde hair and tatooed blue eyes. He may pretend to follow mein Fuhrer, but his jew nose gives him away! Sign the petition to send him to the New World Order Concentration Camp! Heil Fuhrer Oliver! Heil Fuhrer Oliver! Heil Fuhrer Oliver! Heil Fuhrer Oliver!";
+
+// Get the message element from the HTML
+const messageElement = document.getElementById("message-text");
+
+// Set the text content of the message element
+messageElement.textContent = message;
